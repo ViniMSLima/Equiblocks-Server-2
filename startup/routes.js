9655e4327cca892f0c7378ced9cf862da36aac48
ViniMSLima <vinisary.lima@gmail.com>
2024-05-05 11:42:22 -0300

    test server is BO

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const player2 = require("../src/routes/player2");
 const challenge = require("../src/routes/challenge");
-const test = require("../src/routes/test");
 
 module.exports = function (app) 
 {
@@ -9,5 +8,4 @@ module.exports = function (app)
     .use(express.json())
     .use("/api", player2)
     .use("/challenge", challenge)
-    .use("/test", test)
 };
